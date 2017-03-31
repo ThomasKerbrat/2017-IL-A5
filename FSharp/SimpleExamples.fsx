f24532f2cf34b342b2a0d30015d973d8e5148be0
Antoine Raquillet <antoine.raquillet@invenietis.com>
2017-03-31 12:01:16 +0200

    Added first exercise.

---
+++ FSharp/SimpleExamples.fsx
@@ -0,0 +1,45 @@
+﻿let unitPrice = 20
+let items = 5
+let total = 5 * 12
+
+let isNegative = fun x -> x < 0
+isNegative total
+
+(fun x -> x < 0) total
+
+let f = fun x ->
+    match x with
+    | 20 -> -20
+    | 30 -> 60
+    | _ -> x
+
+printfn "%d" (f 20)
+printfn "%d" (f 30)
+printfn "%d" (f 40)
+
+(fun n ->
+    match n with
+    | _ when n < 0 -> -n
+    | _ when n = 1 -> 2
+    | _ -> n) 1
+
+// Exercise 1
+
+let isMultipleOf5 =
+    fun x -> x%5 = 0
+
+let display =
+    fun x ->
+        match isMultipleOf5 x with
+        | true -> printfn "%i multiple de 5" x
+        | false -> printfn "%i pas multiple de 5" x
+
+let rec processUntil100 =
+    fun x ->
+        match x <= 100 with
+        | true -> 
+            display x
+            processUntil100 (x+1)
+        | false -> () 
+
+processUntil100 1
